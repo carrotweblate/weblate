91a1ac9bb4d00abeb24c71269b691426c78658f5
addonion <addonion@gmail.com>
2020-09-04 12:26:55 +0500

    update

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,51 +5,50 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  // pathPrefix: '/blog/article/',
 
-  siteName: 'Carrot quest',
-  titleTemplate: "%s",
+	siteName: 'Carrot quest',
+	titleTemplate: "%s",
 
-  transformers: {
-      remark: {
-          externalLinksTarget: '_blank',
-          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-          anchorClassName: 'icon icon-link',
-          plugins: [
-              // ...global plugins
-          ]
-      }
-  },
+	transformers: {
+		remark: {
+			externalLinksTarget: '_blank',
+			externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+			anchorClassName: 'icon icon-link',
+			plugins: [
+				// ...global plugins
+			]
+		}
+	},
 
-  plugins: [
-      {
-          use: 'gridsome-plugin-gtm',
-          options: {
-              //id: 'GTM-PHNG63V',  //CQ
-              id: 'GTM-TKFL867',  //Пустой
-              enabled: true,
-              debug: false
-          }
-      },
-      {
-          use: 'gridsome-plugin-pwa',
-          options: {
-              // Service Worker Options
-              disableServiceWorker: false,
-              serviceWorkerPath: 'service-worker.js',
-              cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
-              disableTemplatedUrls: false, // Optional
+	plugins: [
+		{
+			use: 'gridsome-plugin-gtm',
+			options: {
+				//id: 'GTM-PHNG63V',  //CQ
+				id: 'GTM-TKFL867',  //Пустой
+				enabled: true,
+				debug: false
+			}
+		},
+		{
+			use: 'gridsome-plugin-pwa',
+			options: {
+				// Service Worker Options
+				disableServiceWorker: false,
+				serviceWorkerPath: 'service-worker.js',
+				cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+				disableTemplatedUrls: false, // Optional
 
-              // Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
-              manifestPath: 'manifest.json',
-              title: 'Carrot quest',
-              startUrl: 'https://www.carrotquest.io/',
-              display: 'standalone',
-              statusBarStyle: 'default',
-              themeColor: '#ff892a',
-              backgroundColor: '#ffffff',
-              icon: './static/favicon.png'
-          }
-      }
-  ],
+				// Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
+				manifestPath: 'manifest.json',
+				title: 'Carrot quest',
+				startUrl: 'https://cq-test.ru/',
+				display: 'standalone',
+				statusBarStyle: 'default',
+				themeColor: '#ff892a',
+				backgroundColor: '#ffffff',
+				icon: './static/favicon.png'
+			}
+		}
+	],
 }
