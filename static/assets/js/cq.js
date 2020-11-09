1355a07fe22a9f17b3e5c34c6a4f7504d692b15c
Nikolay Abdullin <sonyblack@Nikolays-iMac.local>
2020-11-09 17:03:24 +0500

    update

--- static/assets/js/cq.js
+++ static/assets/js/cq.js
@@ -23,31 +23,3 @@ carrotquest.connect('100-675e2c8860097948c325b680f4da');
 carrotquest.track('Зашел на лендинг', {
     'URL': location.href
 });
-
-if ('loading' in HTMLImageElement.prototype) {
-    const images = document.querySelectorAll('img[loading="lazy"]');
-    images.forEach(img => {
-    img.src = img.dataset.src;
-    });
-} else {
-    // Dynamically import the LazySizes library
-    const script = document.createElement('script');
-    script.src ='https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
-    document.body.appendChild(script);
-}
-
-//GTM
-// (function(w, d, s, l, i) {
-//     w[l] = w[l] || [];
-//     w[l].push({
-//         'gtm.start': new Date().getTime(),
-//         event: 'gtm.js'
-//     });
-//     var f = d.getElementsByTagName(s)[0],
-//         j = d.createElement(s),
-//         dl = l != 'dataLayer' ? '&l=' + l : '';
-//     j.async = true;
-//     j.src =
-//         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-//     f.parentNode.insertBefore(j, f);
-// })(window, document, 'script', 'dataLayer', 'GTM-PHNG63V');
