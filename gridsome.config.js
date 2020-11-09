23243950440f77d4b9a08ea9290c8f0d68337c97
Nikolay Abdullin <sonyblack@Nikolays-iMac.local>
2020-11-09 13:23:00 +0500

    update

--- gridsome.config.js
+++ gridsome.config.js
@@ -23,35 +23,35 @@ module.exports = {
 	},
 
 	plugins: [
-		// {
-		// 	use: 'gridsome-plugin-gtm',
-		// 	options: {
-		// 		// id: 'GTM-PHNG63V',  //CQ
-		// 		id: 'GTM-TKFL867',  //Пустой
-		// 		enabled: true,
-		// 		debug: false
-		// 	}
-		// },
-		// {
-		// 	use: 'gridsome-plugin-pwa',
-		// 	options: {
-		// 		// Service Worker Options
-		// 		disableServiceWorker: false,
-		// 		serviceWorkerPath: 'service-worker.js',
-		// 		cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
-		// 		disableTemplatedUrls: true, // Optional
+		{
+			use: 'gridsome-plugin-gtm',
+			options: {
+				// id: 'GTM-PHNG63V',  //CQ
+				id: 'GTM-TKFL867',  //Пустой
+				enabled: true,
+				debug: false
+			}
+		},
+		{
+			use: 'gridsome-plugin-pwa',
+			options: {
+				// Service Worker Options
+				disableServiceWorker: false,
+				serviceWorkerPath: 'service-worker.js',
+				cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+				disableTemplatedUrls: true, // Optional
 
-		// 		// Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
-		// 		manifestPath: 'manifest.json',
-		// 		title: 'Carrot quest',
-		// 		startUrl: 'https://www.carrotquest.io/blog/article/',
-		// 		display: 'standalone',
-		// 		statusBarStyle: 'default',
-		// 		themeColor: '#ff892a',
-		// 		backgroundColor: '#ffffff',
-		// 		icon: './static/favicon.png'
-		// 	}
-		// },
+				// Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
+				manifestPath: 'manifest.json',
+				title: 'Carrot quest',
+				startUrl: 'https://www.carrotquest.io/blog/article/',
+				display: 'standalone',
+				statusBarStyle: 'default',
+				themeColor: '#ff892a',
+				backgroundColor: '#ffffff',
+				icon: './static/favicon.png'
+			}
+		},
 		{
 			use: 'gridsome-source-graphql',
 			options: {
