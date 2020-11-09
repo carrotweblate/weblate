c0a76bd9a2287ac7572bc16f4fc8de9bbae4a5ef
Nikolay Abdullin <sonyblack@Nikolays-iMac.local>
2020-11-09 16:10:19 +0500

    update

--- static/assets/js/cq.js
+++ static/assets/js/cq.js
@@ -25,17 +25,17 @@ carrotquest.track('Зашел на лендинг', {
 });
 
 //GTM
-(function(w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PHNG63V');
+// (function(w, d, s, l, i) {
+//     w[l] = w[l] || [];
+//     w[l].push({
+//         'gtm.start': new Date().getTime(),
+//         event: 'gtm.js'
+//     });
+//     var f = d.getElementsByTagName(s)[0],
+//         j = d.createElement(s),
+//         dl = l != 'dataLayer' ? '&l=' + l : '';
+//     j.async = true;
+//     j.src =
+//         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+//     f.parentNode.insertBefore(j, f);
+// })(window, document, 'script', 'dataLayer', 'GTM-PHNG63V');
