54f81d643acf18edb1e6c85e2c0fc93daf98298f
addonion <addonion@gmail.com>
2020-12-04 16:59:08 +0500

    Update

---
+++ src/pages/Newyear2021.vue
@@ -0,0 +1,37 @@
+<template>
+	<Layout class="page page-index">
+
+		<b-container>
+			
+		</b-container>
+
+	</Layout>
+</template>
+
+<script>
+	export default {
+		//Делаем в HEAD
+		metaInfo() {
+			return {
+				title: 'Инструменты для увеличения продаж на сайтах без привлечения нового трафика',
+				meta: [
+					{
+						key: 'description',
+						name: 'description',
+						content: 'Carrot quest — сервис для увеличения продаж, автоматизации маркетинга, улучшения коммуникации с клиентами ✔️онлайн-консультант ✔️чат-боты ✔️поп-ап окна ✔️триггерные письма ✔️eCRM'
+					},
+					{
+						property: "og:image",
+						content: 'https://www.carrotquest.io/img/cover.gif'
+					},
+				],
+				link: [
+					{
+						rel: 'canonical',
+						href: 'https://www.carrotquest.io/newyear2021/'
+					}
+				]
+			}
+		}
+	}	
+</script>
