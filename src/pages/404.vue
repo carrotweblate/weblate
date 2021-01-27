19fa96e0b9c0f20ea62c1a17c95d2749de76f350
Abdullin Nikolay <addonion@gmail.com>
2021-01-27 16:45:54 +0500

    Update

---
+++ src/pages/404.vue
@@ -0,0 +1,49 @@
+<template>
+	<Layout class="page page-404">
+
+		<b-container>
+            <b-row>
+                <b-col class="pt-5">
+                    <h1>
+                        Страница не найдена
+                    </h1>
+                </b-col>
+            </b-row>
+        </b-container>
+
+	</Layout>
+</template>
+
+<script>
+
+	export default {
+		//Делаем в HEAD
+		metaInfo() {
+			return {
+				title: 'Инструменты для увеличения продаж на сайтах без привлечения нового трафика',
+				meta: [
+					{
+						key: 'description',
+						name: 'description',
+						content: 'Carrot quest — сервис для увеличения продаж, автоматизации маркетинга, улучшения коммуникации с клиентами ✔️онлайн-консультант ✔️чат-боты ✔️поп-ап окна ✔️триггерные письма ✔️eCRM'
+					},
+					{
+						property: "og:image",
+						content: 'https://www.carrotquest.io/assets/images/cover/index.gif'
+					},
+				],
+				link: [
+					{
+						rel: 'canonical',
+						href: 'https://www.carrotquest.io/'
+					}
+				]
+			}
+		}
+	}	
+</script>
+
+
+<style lang="scss">
+
+</style>
