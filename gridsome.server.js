79e85cc366f8f1651adbeca0c1733c03ad02d957
Abdullin Nikolay <addonion@gmail.com>
2021-01-29 12:24:07 +0500

    Back

--- gridsome.server.js
+++ gridsome.server.js
@@ -9,63 +9,63 @@ const axios = require('axios')
 
 module.exports = function (api) {
 	// API from Tilda Files
-	// api.loadSource(async actions => {
-	// 	const { data } = await axios.get(
-	// 		'https://api.tildacdn.info/v1/getproject/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&projectid=62329'
-	// 	)
-	// 	const collection = actions.addCollection('tildaFiles')
-	// 	for (const item of data.result.css) {
-	// 		collection.addNode({css: item})
-	// 	}
-	// 	for (const item of data.result.js) {
-	// 		collection.addNode({js: item})
-	// 	}
-	// })
+	api.loadSource(async actions => {
+		const { data } = await axios.get(
+			'https://api.tildacdn.info/v1/getproject/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&projectid=62329'
+		)
+		const collection = actions.addCollection('tildaFiles')
+		for (const item of data.result.css) {
+			collection.addNode({css: item})
+		}
+		for (const item of data.result.js) {
+			collection.addNode({js: item})
+		}
+	})
 
-	//API from Tilda: Список страниц
-	// api.loadSource(async actions => {
-	// 	const { data } = await axios.get(
-	// 		'https://api.tildacdn.info/v1/getpageslist/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&projectid=62329'
-	// 	)
-	// 	const collection = actions.addCollection('Tilda')
-	// 	api.createManagedPages(async ({ createPage }) => {
-	// 		for (const item of data.result) {
-	// 			if ( ( item.id != '312699' ) && ( item.id == '11937240' || item.id == '16075876' || item.id == '16080876' || item.id == '16083784' || item.id == '2833995' || item.id == '16218892' || item.id == '11880600' || item.id == '9970780' || item.id == '10518279' || item.id == '10714391' || item.id == '10714999' || item.id == '10715060') ) {
-	// 			//if ( item.id != '312699' ) {
-	// 				const { data } = await axios.get(
-	// 					'https://api.tildacdn.info/v1/getpage/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&pageid=' + item.id
-	// 				)
-	// 				if ( item.alias ) {
-	// 					createPage({
-	// 						path: `/${item.alias}`,
-	// 						component: './src/templates/Tilda.vue',
-	// 						context: {
-	// 							id: item.id,
-	// 							title: item.title,
-	// 							description: item.descr,
-	// 							cover: item.img,
-	// 							slug: item.alias,
-	// 							html: data.result.html
-	// 						}
-	// 					})
-	// 				} else {
-	// 					createPage({
-	// 						path: `/${item.filename}`,
-	// 						component: './src/templates/Tilda.vue',
-	// 						context: {
-	// 							id: item.id,
-	// 							title: item.title,
-	// 							description: item.descr,
-	// 							cover: item.img,
-	// 							slug: item.filename,
-	// 							html: data.result.html
-	// 						}
-	// 					})
-	// 				}
-	// 			}
-	// 		}
-	// 	})
-	// })
+	// API from Tilda: Список страниц
+	api.loadSource(async actions => {
+		const { data } = await axios.get(
+			'https://api.tildacdn.info/v1/getpageslist/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&projectid=62329'
+		)
+		const collection = actions.addCollection('Tilda')
+		api.createManagedPages(async ({ createPage }) => {
+			for (const item of data.result) {
+				if ( ( item.id != '312699' ) && ( item.id == '11937240' || item.id == '16075876' || item.id == '16080876' || item.id == '16083784' || item.id == '2833995' || item.id == '16218892' || item.id == '11880600' || item.id == '9970780' || item.id == '10518279' || item.id == '10714391' || item.id == '10714999' || item.id == '10715060') ) {
+				//if ( item.id != '312699' ) {
+					const { data } = await axios.get(
+						'https://api.tildacdn.info/v1/getpage/?publickey=h6wlwdtglx70dzkz1fnn&secretkey=cz7a318b3jpkqm6nzz4l&pageid=' + item.id
+					)
+					if ( item.alias ) {
+						createPage({
+							path: `/${item.alias}`,
+							component: './src/templates/Tilda.vue',
+							context: {
+								id: item.id,
+								title: item.title,
+								description: item.descr,
+								cover: item.img,
+								slug: item.alias,
+								html: data.result.html
+							}
+						})
+					} else {
+						createPage({
+							path: `/${item.filename}`,
+							component: './src/templates/Tilda.vue',
+							context: {
+								id: item.id,
+								title: item.title,
+								description: item.descr,
+								cover: item.img,
+								slug: item.filename,
+								html: data.result.html
+							}
+						})
+					}
+				}
+			}
+		})
+	})
 
 	// //API from Wordpress: Список постов
 	// api.createPages(async ({ graphql, createPage }) =>{
---
+++ src/pages/Sdk.vue
@@ -0,0 +1,48 @@
+<script>
+//Код для sdk (ваще шляпа какая-то, часть удалиться, при переносе страницы на JAM сделаем)
+// if (location.href.indexOf('carrotquest.io/sdk') > 0 && typeof jQuery!='undefined') {
+
+// 	jQuery(document).on('click', '.api-send-button-link', function() {
+// 		carrotquest.track("Лендинг SDK - перешел на github"); 
+// 	});
+// 	function getClosest(el, s) {
+// 		var r = undefined;
+// 		while (el) {
+// 			if (el.matches(s)) {
+// 				r = el;
+// 				break;
+// 			} else if (el.tagName.toLowerCase()=='body') {
+// 				break;
+// 			};
+// 			el = el.parentElement;
+// 		};
+// 		return r;
+// 	};
+// 	var name, app, phone, email;
+// 	document.addEventListener('mousedown', function(e) {
+// 		if (e.target.matches('form.lead-info__form button')) {
+// 			var form = getClosest(e.target, 'form');
+// 				name = form.querySelector('input[name="name"]').value;
+// 				app = form.querySelector('input[name="app"]').value;
+// 				phone = form.querySelector('input[name="phone"]').value;
+// 				email = form.querySelector('input[name="email"]').value;
+// 		};
+// 	});
+// 	document.addEventListener('submit', function(e) {
+// 		if (e.target.matches('form.lead-info__form')) {
+// 			carrotquest.track("Лендинг SDK - записался на демо", {
+// 				'$app': app,
+// 				'$name': name,
+// 				'$phone': phone,
+// 				'$email': email
+// 			});
+// 			carrotquest.identify({
+// 				'$app': app,
+// 				'$name': name,
+// 				'$phone': phone,
+// 				'$email': email
+// 			});
+// 		};
+// 	});
+// }
+</script>
