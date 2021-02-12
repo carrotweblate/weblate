43b06ac8203aa90e613d2152d2f33aa301922af9
Abdullin Nikolay <addonion@gmail.com>
2021-02-12 15:17:14 +0500

    Update

--- gridsome.config.js
+++ gridsome.config.js
@@ -30,48 +30,48 @@ module.exports = {
 	// },
 
 	plugins: [
-		// {
-		// 	use: 'gridsome-plugin-gtm',
-		// 	options: {
-		// 		id: 'GTM-PHNG63V',
-		// 		enabled: true,
-		// 		debug: false
-		// 	}
-		// },
-		// {
-		// 	use: 'gridsome-plugin-pwa',
-		// 	options: {
-		// 		// Service Worker Options
-		// 		disableServiceWorker: false,
-		// 		serviceWorkerPath: 'service-worker.js',
-		// 		cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif,webm,mp4',
-		// 		disableTemplatedUrls: false,
+		{
+			use: 'gridsome-plugin-gtm',
+			options: {
+				id: 'GTM-PHNG63V',
+				enabled: true,
+				debug: false
+			}
+		},
+		{
+			use: 'gridsome-plugin-pwa',
+			options: {
+				// Service Worker Options
+				disableServiceWorker: false,
+				serviceWorkerPath: 'service-worker.js',
+				cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif,webm,mp4',
+				disableTemplatedUrls: false,
 	
-		// 		// Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
-		// 		manifestPath: 'manifest.json',
-		// 		title: 'Carrot quest',
-		// 		description: 'Инструменты для увеличения продаж на сайтах без привлечения нового трафика',
-		// 		shortName: 'CQ',
-		// 		startUrl: '/',
-		// 		display: 'standalone',
-		// 		statusBarStyle: 'default',
-		// 		themeColor: '#FF7C16',
-		// 		backgroundColor: '#ffffff',
-		// 		icon: './static/favicon.png',
-		// 		maskableIcon: true,
-		// 		svgFavicon: './static/favicon.svg',
-		// 		appleMaskIcon: './static/favicon.svg',
-        //     	appleMaskIconColor: '#ffffff',
-		// 	}
-		// },
-		// {
-		// 	use: '@gridsome/plugin-critical',
-		// 	options: {
-		// 		paths: ['/'],
-		// 		width: 1300,
-		// 		height: 900
-		// 	}
-		// },
+				// Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
+				manifestPath: 'manifest.json',
+				title: 'Carrot quest',
+				description: 'Инструменты для увеличения продаж на сайтах без привлечения нового трафика',
+				shortName: 'CQ',
+				startUrl: '/',
+				display: 'standalone',
+				statusBarStyle: 'default',
+				themeColor: '#FF7C16',
+				backgroundColor: '#ffffff',
+				icon: './static/favicon.png',
+				maskableIcon: true,
+				svgFavicon: './static/favicon.svg',
+				appleMaskIcon: './static/favicon.svg',
+            	appleMaskIconColor: '#ffffff',
+			}
+		},
+		{
+			use: '@gridsome/plugin-critical',
+			options: {
+				paths: ['/'],
+				width: 1300,
+				height: 900
+			}
+		}
 		// {
 		// 	use: 'gridsome-source-graphql',
 		// 	options: {
