7bd0b24102992382b64407acae7606f40a5f46b1
Abdullin Nikolay <addonion@gmail.com>
2021-02-19 18:37:18 +0500

    Update

--- src/pages/Testdrive/Frame.vue
+++ src/pages/Testdrive/Frame.vue
@@ -1,7 +1,7 @@
 <template>
 	<Layout :header="false" :footer="false">
-
-		<img id="siteImage" :src="searchSite" width="100%" alt=" ">
+		
+		<img v-if="site" id="siteImage" :src="site" width="100%" :alt="site">
 
 	</Layout>
 </template>
@@ -14,39 +14,37 @@
 		data: function() {
 			return {
 				//searchSite: 'https://api.apiflash.com/v1/urltoimage?access_key=f883a85678514489bf673efe63f814fe&format=jpeg&full_page=true&width=1400&quality=80&url='
-				searchSite: ''
+				searchSite: '',
+				site: ''
+			}
+		},
+		created: function () {
+			//Достаём урл
+			var search = location.search.substr(1)
+				.split('&')
+				.reduce(function(res, a) {
+					var t = a.split('=');
+					res[decodeURIComponent(t[0])] = t.length == 1 ? null : decodeURIComponent(t[1]);
+					return res;
+				}, {});
+			this.searchSite = search.site
+			if (!!this.searchSite.search('http')) {
+				if (!this.searchSite.search('http://')) {
+					this.searchSite = this.searchSite.replace('http://','https://')
+				} else {
+					this.searchSite = 'https://' + this.searchSite
+				}
 			}
 		},
 		//Грузим изображение
 		async mounted() {
 			try {
-				axios.get('https://services.eu-central-1.v2.cloudbrowser-api.com/image?source=http://yandex.ru&key=h5QYilxGGJ2dRWV48GCn4yK0i4GFLbNFphtxGgdnCop26nj9bZ1wGVUvdJ0mwRoviPLa3ZcB', {
-					headers: {
-						'Access-Control-Allow-Origin': '*',
-						'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-						'Access-Control-Allow-Headers': 'Authorization'
-					}
-					}).then(function (response) {
-						console.log('response is : ' + response.data);
-					}).catch(function (error) {
-					}
-				);
+				axios.get('https://cors-anywhere.herokuapp.com/https://services.eu-central-1.v2.cloudbrowser-api.com/image?output_return_method=2&client_width=1400&capture_full_page=1&key=h5QYilxGGJ2dRWV48GCn4yK0i4GFLbNFphtxGgdnCop26nj9bZ1wGVUvdJ0mwRoviPLa3ZcB&source=' + this.searchSite).then(ref => {
+					this.site = ref.data.location
+				})
 			} catch (error) {
+				console.log(error)
 			}
-		},
-		// mounted () {
-		// 	//Достаём урл
-		// 	var search = location.search.substr(1)
-		// 		.split('&')
-		// 		.reduce(function(res, a) {
-		// 			var t = a.split('=');
-		// 			res[decodeURIComponent(t[0])] = t.length == 1 ? null : decodeURIComponent(t[1]);
-		// 			return res;
-		// 		}, {});
-			
-		// 	// if (search.site) {
-		// 	// 	this.searchSite = this.searchSite + search.site
-		// 	// }
-		// }
+		}
 	}
 </script>
