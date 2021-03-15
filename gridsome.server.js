123ac0464fd96f114dbcfaa0c4bc25331fe3f9ed
Abdullin Nikolay <addonion@gmail.com>
2021-03-15 14:54:57 +0500

    Withoout GTM

--- gridsome.server.js
+++ gridsome.server.js
@@ -86,7 +86,7 @@ module.exports = function (api) {
 	// API Wordpress - создаём Посты
 	api.loadSource(async actions => {
 		const { data } = await axios.get(
-			'https://www.carrotquest.io/blog/wp-json/wp/v2/posts?&per_page=1'
+			'https://www.carrotquest.io/blog/wp-json/wp/v2/posts?&per_page=999'
 		)
 		// Данные для вывода статей
 		const collection = actions.addCollection('post')
