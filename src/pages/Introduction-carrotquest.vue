45792712556f2767f2f851e4f4af61e8724f93b6
SerezhaSlizh <81413524+SerezhaSlizh@users.noreply.github.com>
2021-03-26 15:17:11 +0500

    Актуализировал ссылки на кейсы
    С https://www.carrotquest.io/blog/kejs-kak-dom-ru-uvelichil-konversiyu-sajta-na-30-s-pomoshhyu-carrot-quest/ на /blog/case-dom-ru-konversii/
    С https://www.carrotquest.io/blog/kak-organizovana-sluzhba-podderzhki-v-modulbank-i-pochemu-oni-ne-boyatsya-davat-obeshhaniya-o-skorosti-otveta/ на /blog/case-modulbank/
    С https://www.carrotquest.io/blog/case_skyshop/ на /blog/case-skyshop/

--- src/pages/Introduction-carrotquest.vue
+++ src/pages/Introduction-carrotquest.vue
@@ -698,21 +698,21 @@
 						logo: 'modulbank.svg',
 						title: 'обращений в чат обрабатывают ежедневно',
 						text: '<p>Carrot quest помогает Модульбанку организовать поддержку и&nbsp;продажи через чат. Благодаря распределению диалогов и&nbsp;объединению каналов в&nbsp;одном месте команда Модульбанка снизила скорость первого ответа до&nbsp;10&nbsp;минут.</p>',
-						href: '/blog/kak-organizovana-sluzhba-podderzhki-v-modulbank-i-pochemu-oni-ne-boyatsya-davat-obeshhaniya-o-skorosti-otveta/'
+						href: '/blog/case-modulbank/'
 					},
 					{ 
 						number: 'На 30%' ,
 						logo: 'domru.svg',
 						title: 'увеличилась конверсия сайта',
 						text: '<p>Carrot&nbsp;quest помогает <a href="https://domru.ru">Дом.ru</a> собирать лиды и&nbsp;увеличивать продажи с&nbsp;помощью триггерных поп-апов, писем и&nbsp;пуш-уведомлений. В&nbsp;ходе работы внедрили больше 49&nbsp;сценариев, а&nbsp;количество заявок увеличилось на&nbsp;30%.</p>',
-						href: '/blog/kejs-kak-dom-ru-uvelichil-konversiyu-sajta-na-30-s-pomoshhyu-carrot-quest/'
+						href: '/blog/case-dom-ru-konversii/'
 					},
 					{ 
 						number: '31,3%',
 						logo: 'skyshop.svg',
 						title: 'сделали заказ из рассылки с акциями SkyShop',
 						text: '<p>Carrot quest помогает SkyShop найти слабые стороны и пересмотреть способы работы с пользователями. После настройки ручной рассылки с акциями 31,3% пользователя сделали заказ. Еще 20564 пользовался оставили email в поп-апах.</p>',
-						href: '/blog/case_skyshop/'
+						href: '/blog/case-skyshop/'
 					},
 					{ 
 						number: 'На 40%',
