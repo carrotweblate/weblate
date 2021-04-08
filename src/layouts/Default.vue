052981c59b21ab62d06c18637f410d83207af5c5
addonion <addonion@gmail.com>
2021-04-08 10:36:28 +0500

    Добавил шрифт в прелоад

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -395,6 +395,13 @@
 						type: 'font/woff2', 
 						crossorigin: 'anonymous'
 					},
+					{
+						rel: 'preload',
+						href: 'https://cdn-www.carrotquest.io/assets/font/OpenSans-Bold.woff2',
+						as: 'font', 
+						type: 'font/woff2', 
+						crossorigin: 'anonymous'
+					},
 					// Подключаем Stag sans
 					{
 						rel: 'preload',
