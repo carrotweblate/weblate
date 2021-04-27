57426aeea3d5ba9b1d7b3a6d936c765875f9e2f8
Abdullin Nikolay <addonion@gmail.com>
2021-04-27 12:30:19 +0500

    CQ-redirection.map

--- src/templates/Tilda.vue
+++ src/templates/Tilda.vue
@@ -52,7 +52,7 @@
 				if (value.node.css) {
 					tildaLink.push({ rel: 'stylesheet' , href: value.node.css })
 				} else {
-					tildaScripts.push({ src: value.node.js })
+					tildaScripts.push({ src: value.node.js , defer: 'defer' })
 				}
 			}
 			return {
@@ -107,6 +107,9 @@
 <style lang="scss">
 	.page-tilda {
 		overflow: hidden;
+		*, :after, :before {
+			box-sizing: content-box !important;
+		}
 		#t-header, #t-footer {
 			display: none;
 		}
