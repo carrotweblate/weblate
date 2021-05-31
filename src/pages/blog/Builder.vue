15319c59ad0ea210e5186f15b4fbfffcb9777342
Abdullin Nikolay <addonion@gmail.com>
2021-05-31 18:08:54 +0500

    Update

---
+++ src/pages/blog/Builder.vue
@@ -0,0 +1,20 @@
+<template>
+	<Layout :header="true" :footer="true"  class="page blog-index">
+
+		<b-container>
+			<b-row>
+				<b-col>
+					<h1 class="my-5">
+						Лучшие статьи
+					</h1>
+				</b-col>
+			</b-row>
+			<b-row>
+				<b-col>
+                    <a href="#open-modal-consultation">Ссылка</a>
+                </b-col>
+			</b-row>
+		</b-container>
+
+	</Layout>
+</template>
